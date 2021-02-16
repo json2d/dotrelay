f7d8bbc119b91abcefa6be7d89f666a022eeb921
Jason Yung <json.yung@gmail.com>
2021-02-15 19:30:20 -0500

    abandon use of context manager
    + becasue removing a module import path could be problematic

--- NEXT.md
+++ NEXT.md
@@ -1,4 +1,13 @@
 # Whats next
 
+
+## NEXT
+```py
+import dotrelay
+dotrelay.init(__file__) # very clean
+
+dotrelay.flush() # 👃 better not though - this could potentially and unintentionally remove a module import path that was already there and should stay there!
+```
+
 > ### Disclaimer 
 > The contents of this file is a fairly coherent stream of conscience brainstorm about different direction to take the design of this library, and intended to be documentation with accurate code examples that reflect how you would actually use the library. For that refer to `README.md`
--- dotrelay/core.py
+++ dotrelay/core.py
@@ -7,30 +7,21 @@ import sys
 
 MAX_DEPTH = 10
 
-# use context manager
-class Relay():
-  def __init__(self, path, max_depth=MAX_DEPTH):
-    self.path = os.path.abspath(path)
-    self.max_depth = max_depth
-    self.mod_path = None
-  
-  def __enter__(self):
-    curr_path = self.path
-    for depth in range(0, self.max_depth):
-      relay_file_path = os.path.join(curr_path, '.relay')
-      if depth > 0 and os.path.exists(relay_file_path):
-        log.debug(f'depth of {depth} reached - .relay file found in {self.mod_path} - adding to module import path...')
-        self.mod_path = curr_path
-        sys.path.append(self.mod_path)
-        break
-      else:
-        log.debug(f'depth of {depth} reached - .relay file not found in {curr_path} - checking parent path...')
-        curr_path = os.path.dirname(curr_path) # go up to parent path
+def init(path, max_depth=MAX_DEPTH):
+  path = os.path.abspath(path)
+  mod_path = None
+  curr_path = path
 
-    if not self.mod_path:    
-      log.warn(f'max depth of {depth} reached - .relay file not found in any ancestor paths - no changes were made to module import path.')
+  for depth in range(0, max_depth):
+    relay_file_path = os.path.join(curr_path, '.relay')
+    if depth > 0 and os.path.exists(relay_file_path):
+      log.info(f'depth of {depth} reached - .relay file found in {mod_path} - adding to module import path...')
+      mod_path = curr_path
+      sys.path.append(mod_path)
+      break
+    else:
+      log.info(f'depth of {depth} reached - .relay file not found in {curr_path} - checking parent path...')
+      curr_path = os.path.dirname(curr_path) # go up to parent path
 
-  def __exit__(self, type, value, traceback):
-    if self.mod_path:
-      log.debug(f'finished relaying {self.mod_path} to {self.path} - removing from module import path...')
-      sys.path.remove(self.mod_path)
+  if not mod_path:    
+    log.warn(f'max depth of {depth} reached - .relay file not found in any ancestor paths - no changes were made to module import path.')
